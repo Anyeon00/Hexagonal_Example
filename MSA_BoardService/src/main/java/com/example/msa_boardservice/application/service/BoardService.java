7069ed65494e4b1ce989e07d700041abb7e596b2
Anyeon00 <110832536+Anyeon00@users.noreply.github.com>
2024-11-21 14:40:09 +0900

    Add files via upload

--- MSA_BoardService/src/main/java/com/example/msa_boardservice/application/service/BoardService.java
+++ MSA_BoardService/src/main/java/com/example/msa_boardservice/application/service/BoardService.java
@@ -4,6 +4,7 @@ import com.example.msa_boardservice.application.port.out.BoardRepository;
 import com.example.msa_boardservice.application.domain.Board;
 import com.example.msa_boardservice.application.port.in.BoardUsecase;
 import com.example.msa_boardservice.application.port.in.dto.BoardWriteInfo;
+import com.example.msa_boardservice.application.service.component.BoardAppender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,28 +17,35 @@ public class BoardService implements BoardUsecase {
 
     private final BoardRepository boardRepository;
 
+    private final BoardAppender boardAppender;
+//    private final BoardReader boardReader;
+//    private final BoardUpdater boardUpdater;
+//    private final BoardDeleter boardDeleter;
+
     @Override
     @Transactional
     public Long write(BoardWriteInfo info) {
-        Long save = boardRepository.save(
-                Board.create(info.title(), info.content()));
+        Long save = boardAppender.append(Board.create(info.title(), info.content()));
         return save;
     }
 
     @Override
     public Optional<Board> find(Long boardId) {
+//        return boardReader.read(boardId);
         return boardRepository.findById(boardId);
     }
 
     @Override
     @Transactional
     public Long update(Long boardId, BoardWriteInfo info) {
+//        return boardUpdater.update(boardId, info);
         return boardRepository.update(boardId, info);
     }
 
     @Override
     @Transactional
     public void delete(Long boardId) {
+//        boardDeleter.delete(boardId);
         boardRepository.delete(boardId);
     }
 }
